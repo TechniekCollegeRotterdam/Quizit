273d293e89eeec5b7d5f09e5e02cd512f9dead0c
Ole van der Heiden <olejonasvanderheiden@gmail.com>
2020-10-13 13:49:06 +0200

    Added moddels  --all for Answer/Game/Qustion/Gameanswer

---
+++ database/seeds/GameanswerSeeder.php
@@ -0,0 +1,16 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+class GameanswerSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        //
+    }
+}
