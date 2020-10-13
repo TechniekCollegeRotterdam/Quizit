9fa07a49fd522fcfa6ae8560b93c5a94ff9b7ecb
TJSproducties <tjsprodukties@gmail.com>
2020-10-13 14:16:44 +0200

    renewed delete.blade.php Made basic index.blade.php layout for quizzes Started with quizzes layout for create.blade.php

--- resources/views/admin/quizzes/create.blade.php
+++ resources/views/admin/quizzes/create.blade.php
@@ -1,2 +1,20 @@
-<?php
+@extends('Layouts.layout')
 
+@section('content')
+    <h1 class="mt-5">Quizzes</h1>
+
+    <form method="POST" action="{{ route() }}">
+        @csrf
+
+        <div class="form-group">
+            <label for="name">Quiznaam</label>
+            <input type="text" name="name" class="form-control" id="name"
+                   aria-describedby="quiznameHelp" placeholder="Voer de quiznaam in">
+        </div>
+
+        <label for="description">Quiz omschrijving</label>
+        <input type="text" name="description" class="form-control" id="name"
+                aria-describedby="quizdescriptionHelp" placeholder="Voer een quiz omschrijving in">
+
+    </form>
+@endsection
