40cb7b337b0fb7a55a84fe3fe49aaebd6c850777
TJSproducties <tjsprodukties@gmail.com>
2020-11-27 10:36:14 +0100

    gitflow-feature-stash: fixes
    userseeder fixes

--- app/User.php
+++ app/User.php
@@ -5,6 +5,7 @@ namespace App;
 use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
+use Spatie\Permission\Traits\HasRoles;
 
 class User extends Authenticatable
 {
@@ -14,6 +15,8 @@ class User extends Authenticatable
     }
 
     use Notifiable;
+    use HasRoles;
+
 
     /**
      * The attributes that are mass assignable.
