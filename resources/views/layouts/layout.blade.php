af776959d902eb9a3267e387d1aae18d05cd5746
TJSproducties <tjsprodukties@gmail.com>
2020-11-30 15:19:45 +0100

    fixed weird head

--- resources/views/layouts/layout.blade.php
+++ resources/views/layouts/layout.blade.php
@@ -56,10 +56,8 @@
                 <li class="nav-item">
                     <a class="nav-link" href="{{route('game.index')}}">Game</a>
                 </li>
-<<<<<<< HEAD
                 @endhasanyrole
-=======
->>>>>>> 2663857... gitflow-feature-stash: hasanyrole
+
 
 
                 @hasanyrole('admin')
@@ -76,10 +74,7 @@
                     </div>
                 </li>
                 @endhasanyrole
-<<<<<<< HEAD
 
-=======
->>>>>>> 2663857... gitflow-feature-stash: hasanyrole
                 <li class="nav-item float-right">
                     <ul class="navbar-nav ml-auto">
                         <!-- Authentication Links -->
